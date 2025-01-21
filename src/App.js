25a8e42ae5a15a520d71318370ead11ea503e46a
Park Ji Hyun <030226jihyun@naver.com>
2025-01-21 21:16:02 +0900

    feat: 기본 기능 및 className 추가

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import React, {useState} from "react";
 
 function App() {
 
@@ -8,9 +9,19 @@ function App() {
   };
 
 
-  //  useEffect(()=>{
-  //   console.log("!!!")
-  // });
+  const [display, setDisplay] = useState('0');
+  const [watingForOperand, setWatingForOperand] = useState(true);
+
+  const inputDigit = (digit) => {
+    if (watingForOperand) {
+      setDisplay(String(digit));
+      setWatingForOperand(false);
+    } else {
+      setDisplay(display === '0' ? String(digit) : display + digit);
+    }
+  }
+
+  
 
   return (
     <div className="App">
@@ -18,33 +29,51 @@ function App() {
   
         <p>계산기</p>
 
-        <input
+        <input readonly
         type="number"
+        value={display}
+        readOnly
         onChange={handlesNumber}
         >
 
         </input>
         
-        <div className="numbers">
-      <button>1</button>
-      <button>2</button>
-      <button>3</button>
-      <button>4</button>
-      <button>5</button>
-      <button>6</button>
-      <button>7</button>
-      <button>8</button>
-      <button>9</button>
-      <button>9</button>
+      <div className="numbers">
+        <div className="row">
+          <button id="num-1">1</button>
+          <button id="num-2">2</button>
+          <button id="num-3">3</button>
+        </div>
+
+        <div className="row">
+          <button id="num-4">4</button>
+          <button id="num-5">5</button>
+          <button id="num-6">6</button>
+        </div>
+
+        <div className="row">
+          <button id="num-7">7</button>
+          <button id="num-8">8</button>
+          <button id="num-9">9</button>
+        </div>
+        <button id="num-0">0</button>
+
+
         </div>
 
         <div className="operations">
-        <button>+</button>
-        <button>-</button>
-        <button>÷</button>
-        <button>*</button>
-        <button>AC</button>
-        <button>=</button>
+
+          <div className="flex-column">
+            <button id="divide">÷</button>
+            <button id="multiply">x</button>
+            <button id="minus">-</button>
+            <button id="plus">+</button>
+          </div>
+
+            <button id="all-clear">AC</button>
+            <button id="clear">C</button>
+            <button id="calculate">=</button>
+        
 
         </div>
 
